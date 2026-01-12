baab98c73eb81218afea49c353acabb72dac54a8
MedericFourmy <mederic.fourmy@gmail.com>
2026-01-12 15:03:24 +0000

    add rviz to launch file

--- launch/charuco_ros2.launch.py
+++ launch/charuco_ros2.launch.py
@@ -1,9 +1,13 @@
+
+import os
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition
 from ament_index_python.packages import get_package_share_directory
-import os
 
 
 def generate_launch_description():
@@ -15,21 +19,40 @@ def generate_launch_description():
         "params.yaml"
     )
 
-    camera_namespace_arg = DeclareLaunchArgument(
-        "camera_namespace",
-        default_value="camera",
-        description="Namespace for the camera"
-    )
-    
-    camera_name_arg = DeclareLaunchArgument(
-        "camera_name",
-        default_value="camera",
-        description="Name of the camera"
+    rviz_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare("charuco_ros2"),
+            "rviz",
+            "debug.rviz",
+        ]
     )
 
     return LaunchDescription([
-        camera_namespace_arg,
-        camera_name_arg,
+        DeclareLaunchArgument(
+            "camera_namespace",
+            default_value="camera",
+            description="Namespace for the camera"
+        ),
+        DeclareLaunchArgument(
+            "camera_name",
+            default_value="camera",
+            description="Name of the camera"
+        ),
+        DeclareLaunchArgument(
+            "launch_rviz",
+            default_value="false",
+            description="Launch rviz2 with debug configuration"
+        ),
+        DeclareLaunchArgument(
+            "rviz_config_path",
+            default_value=rviz_config_file,
+            description="Path to a file containing RViz view configuration.",
+        ),
+        DeclareLaunchArgument(
+            "use_rviz",
+            default_value="false",
+            description="Launch RViz with default view.",
+        ),
         Node(
             package="charuco_ros2",
             executable="charuco_ros2_node",
@@ -41,5 +64,12 @@ def generate_launch_description():
                 ("image_raw", [LaunchConfiguration("camera_name"), "/color/image_raw"]),
                 ("camera_info", [LaunchConfiguration("camera_name"), "/color/camera_info"]),
             ],
+        ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            arguments=["-d", rviz_config_file],
+            condition=IfCondition(LaunchConfiguration("launch_rviz")),
         )
     ])
--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         (os.path.join('share', package_name), ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
